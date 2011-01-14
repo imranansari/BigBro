6a1bcf2fff4c66d21ce4d1cca4dfbf73eb9d775e
iansari <imran.iansari@gmail.com>
2011-01-14 00:12:28 -0500

    live data

--- public/js/bigbro.js
+++ public/js/bigbro.js
@@ -17,59 +17,66 @@
 
         // Creating the map
         var map = new google.maps.Map(document.getElementById('map'), options);
+        var activities;
 
-        var activity = [
-            {
-                id: '1',
-                user: 'Homer Simpson',
-                application: 'LincPad',
-                event: 'Contract Search',
-                location: '36.072435, -79.791353',
-                lat: '36.072435',
-                lng: '-79.791353'
-            },
-            {
-                id: '2',
-                user: 'Marge Simpson',
-                application: 'LincPad',
-                event: 'Event Search',
-                location: '36.042709, -79.929537',
-                lat: '36.042709',
-                lng:'-79.929537'
-            }
+        $.get('/events', {dataType : 'json'}, function(data) {
+            activities = data;
+            console.log(activities);
 
-        ];
+            /*activity = [
+             {
+             id: '1',
+             user: 'Homer Simpson',
+             application: 'LincPad',
+             event: 'Contract Search',
+             location: '36.072435, -79.791353',
+             lat: '36.072435',
+             lng: '-79.791353'
+             },
+             {
+             id: '2',
+             user: 'Marge Simpson',
+             application: 'LincPad',
+             event: 'Event Search',
+             location: '36.042709, -79.929537',
+             lat: '36.042709',
+             lng:'-79.929537'
+             }
 
-
-        jQuery.each(activity, function() {
-
-            var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(this.lat, this.lng),
-                map: map,
-                title: this.user
-            });
+             ];*/
 
 
-            (function(activity, marker) {
-                google.maps.event.addListener(marker, 'click', function() {
+            jQuery.each(activities, function() {
 
-                    function getContent(activity) {
-                        var content = 'User : ' + activity.user +
-                                '</br> Application : ' + activity.application +
-                                '</br> Event : ' + activity.event +
-                                '</br> Location : ' + activity.location;
-                        return content;
-                    }
-
-                    var infowindow = new google.maps.InfoWindow({
-                        content: getContent(activity)
-                    });
-
-                    infowindow.open(map, marker);
-
+                var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(this.activity.lat, this.activity.lng),
+                    map: map,
+                    title: this.activity.user
                 });
 
-            })(this, marker);
+
+                (function(activity, marker) {
+                    google.maps.event.addListener(marker, 'click', function() {
+
+                        function getContent(activity) {
+                            var content = 'User : ' + activity.user +
+                                    '</br> Application : ' + activity.application +
+                                    '</br> Event : ' + activity.event +
+                                    '</br> Location : ' + activity.location;
+                            return content;
+                        }
+
+                        var infowindow = new google.maps.InfoWindow({
+                            content: getContent(activity)
+                        });
+
+                        infowindow.open(map, marker);
+
+                    });
+
+                })(this.activity, marker);
+
+            });
 
         });
 
