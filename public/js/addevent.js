4473fa781e5ac544eeab02a2ccca2e222aad0ebf
iansari <imran.iansari@gmail.com>
2011-01-14 00:12:15 -0500

    checkin

---
+++ public/js/addevent.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+   $('#submitActivity').click(function(){
+       var data = $.toJSON($('#eventForm').serializeObject());
+       $.post("/addevent", data );
+   });
+});
+
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name]) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
