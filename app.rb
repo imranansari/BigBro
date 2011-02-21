09c24f22b8b3fdf1ba9874bb1390eac2c5abb56d
iansari <imran.iansari@gmail.com>
2011-02-21 14:42:04 -0500

    cleanup

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'haml'
 require 'sass'
 require 'json'
 require 'active_record'
+require './configure_db'
 
 get '/app' do
   haml :index
@@ -24,77 +25,14 @@ post '/addactivity' do
   
 end
 
-get '/test' do
-  puts "test hello"
-
-  activity      = Activity.new
-  activity.name = 'homer'
-  puts activity.name
-
-#  puts activity.to_json
-end
-
 get '/activities' do
   content_type 'application/json'
   products = Activity.find(:all)
   products.to_json
 end
 
-get '/css/:name.css' do
-  content_type 'text/css', :charset => 'utf-8'
-  scss(:"/stylesheets/#{params[:name]}")
-end
-
-configure do
-  ActiveRecord::Base.establish_connection(
-      :adapter  => 'sqlite3',
-      :database => './db/bb.db',
-      :timeout  => 5000
-  )
-
-#  class CreateActivities < ActiveRecord::Migration
-#    def self.up
-#      create_table :activities, :force => true do |t|
-#        t.integer :id
-#        t.string :user
-#        t.string :application
-#        t.string :event
-#        t.string :lat
-#        t.string :lng
-#      end
-#    end
-#  end
-#  CreateActivities.up
-
-  class Activity < ActiveRecord::Base
-    validates_uniqueness_of :id
-  end
-  #Activity.create(:id => 1, :user => 'imran', :application => 'LincPad')
-
-##control table##
-
-  class CreateSSEControl < ActiveRecord::Migration
-    def self.up
-      create_table :ssecontrols, :force => true do |t|
-        t.integer :id
-        t.boolean :newevent
-      end
-    end
-  end
-  CreateSSEControl.up
-
-  class Ssecontrol < ActiveRecord::Base
-    validates_uniqueness_of :id
-  end
-
-  Ssecontrol.create(:id => 1, :newevent => true)
-end
-
 get '/pullNewActivity' do
   content_type 'text/event-stream'
-  time1 = Time.new
-  mytime = time1.inspect
-  puts mytime
   newevent = false
   sseControl = Ssecontrol.find(1)
   if sseControl.newevent then
@@ -104,5 +42,9 @@ get '/pullNewActivity' do
   end
 
   return "data: "+newevent.inspect+" \n\n"
+end
 
+get '/css/:name.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  scss(:"/stylesheets/#{params[:name]}")
 end
