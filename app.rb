e774ad75be2b39fe4bf38307cc203e92a93c5279
iansari <imran.iansari@gmail.com>
2011-03-07 19:31:51 -0500

    refactor

--- app.rb
+++ app.rb
@@ -24,38 +24,28 @@ end
 
 post '/activity' do
   puts 'activity posted'
-  activity = JSON.parse(request.body.read)
-  puts activity['lat']
-  georesponse =GoogleGeocoder.reverse_geocode([activity['lat'],activity['lng']])
-
-  activity_rec = Activity.create(activity)
-  activity_rec.loc_street = georesponse.street_address
-  activity_rec.loc_city = georesponse.city
-  activity_rec.loc_state = georesponse.state
-  activity_rec.loc_zipcode = georesponse.zip
-  activity_rec.loc_country = georesponse.country
+  activity     = JSON.parse(request.body.read)
+  activity_rec = setActivityGeolocation(activity)
   activity_rec.save
 
-  sseControl = Ssecontrol.find(1)
-  sseControl.newevent = true
-  sseControl.save
-  
+  setSseFlag()
+
 end
 
 get '/activity' do
   content_type 'application/json'
   response['Expires'] = (Time.now).httpdate
-  activities = Activity.find(:all)
+  activities          = Activity.find(:all)
   puts activities.to_json
   activities.to_json
 end
 
 get '/pullnewactivity' do
   content_type 'text/event-stream'
-  newevent = false
+  newevent   = false
   sseControl = Ssecontrol.find(1)
   if sseControl.newevent then
-    newevent = true
+    newevent            = true
     sseControl.newevent = false
     sseControl.save
   end
@@ -65,10 +55,24 @@ end
 
 get '/css/:name.css' do
   content_type 'text/css', :charset => 'utf-8'
-  response['Expires'] = (Time.now).httpdate  
+  response['Expires'] = (Time.now).httpdate
   scss(:"/stylesheets/#{params[:name]}")
 end
 
-get '/favicon.ico' do
-  "Hello World"
+def setActivityGeolocation(activity)
+  georesponse              =GoogleGeocoder.reverse_geocode([activity['lat'], activity['lng']])
+
+  activity_rec             = Activity.create(activity)
+  activity_rec.loc_street  = georesponse.street_address
+  activity_rec.loc_city    = georesponse.city
+  activity_rec.loc_state   = georesponse.state
+  activity_rec.loc_zipcode = georesponse.zip
+  activity_rec.loc_country = georesponse.country
+  activity_rec
+end
+
+def setSseFlag
+  sseControl          = Ssecontrol.find(1)
+  sseControl.newevent = true
+  sseControl.save
 end
