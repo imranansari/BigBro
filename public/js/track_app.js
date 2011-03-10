5b9570ed44c1bea99e700fd6ee1383b15952c3b1
iansari <imran.iansari@gmail.com>
2011-03-09 21:21:43 -0500

    alert

--- public/js/track_app.js
+++ public/js/track_app.js
@@ -19,6 +19,7 @@ $(document).ready(function() {
     navigator.geolocation.getCurrentPosition(handle_geolocation);
 
     function handle_geolocation(position) {
+        alert(getDevice());
         activityData.lat = position.coords.latitude;
         activityData.lng = position.coords.longitude;
         activityData.deviceType = getDevice();
